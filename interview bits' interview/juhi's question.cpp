c8e3131785af5482d0b34a2743ec25c5f5237a65
Abhay Gupta <nextresp@gmail.com>
2020-06-28 07:12:20 +0530

    all subarrays are getting counted

---
+++ interview bits' interview/juhi's question.cpp
@@ -0,0 +1,41 @@
+//all the arrays with exactly b numbers of odd numbers
+#include<bits/stdc++.h>
+using namespace std;
+
+int totalarrays(int a[],int n,int m){
+    int count = 0; 
+    int prefix[n] = { 0 }; 
+    int odd = 0; 
+  
+    // traverse in the array 
+    for (int i = 0; i < n; i++)  
+    { 
+  
+        prefix[odd]++; 
+  
+        // if array element is odd 
+        if (a[i] & 1) 
+            odd++; 
+  
+        // when number of odd elements>=M 
+        if (odd >= m) 
+            count += prefix[odd - m]; 
+    } 
+  
+    return count;
+}
+
+//driver function
+int main(){
+    int n,A[100000],b;
+
+    //taking inputs from user
+    cin>>n;
+    for(int i=0;i<n;i++)cin>>A[i];
+    cin>>b;
+
+    //calling computing function with inputted values
+    cout<<totalarrays(A,n,b);
+
+    return 0;
+}
