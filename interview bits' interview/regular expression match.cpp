b4f556b9a443b6ed5ba9767f9772fa821d924780
Abhay Gupta <nextresp@gmail.com>
2020-07-04 13:26:16 +0530

    new technique

--- interview bits' interview/regular expression match.cpp
+++ interview bits' interview/regular expression match.cpp
@@ -1,32 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool match(string a,string b,int i,int j,int n,int m){
-    //if a.sub(i)==b.sub(j) return true;
-    if(i>=n && j>=m)return true;
-    if(a.substr(i,n-i)==b.substr(j,m-j))return true;
 
-    if(a[i]==b[j])return (a,b,i+1,j+1,n,m);
+    bool Match(string s, string p) {
+        if(s.size()==0 && p.size()==0)return true;
+        if(s.size()==0 || p.size()==0)return false;
+        if(s==p)return true;
+        //if(s=="aab" && p=="c*a*b")return true;
+        
+        char c=p[0];
+        char d=s[0];
+        p.erase(p.begin()+0);
 
-    if(b[j]=='*'){
-        char c=a[i];
-        bool boo=false;
-        //boo|=match(a,b,i,j+1,n,m);
-        while(a[i]==c && i<n){
-            boo|=match(a,b,i,j+1,n,m);
-            i++;
+        if(c=='.' || c==d){s.erase(s.begin()+0);return Match(s,p);}
+        
+        
+        if(c=='*'){
+            bool b=false;
+            while(s.size()!=0 && s[0]==d && b==false){
+                b|=Match(s,p);
+                s.erase(s.begin()+0);
+            }
+            //s.erase(s.begin()+0);
+            b|=Match(s,p);
+            
+            return b;
         }
-        if(i==n){boo|=match(a,b,i,j+1,n,m);}
-        return boo;
+        
+        return false;
     }
 
-    if(b[j]=='?')return match(a,b,i+1,j+1,n,m);
-
-    return false;
-}
 
 int main(){
-    string a="aaaa",b="*";
-    cout<<match(a,b,0,0,a.length(),b.length());
+    string a="aaaab",b="*b";
+    cout<<Match(a,b);
     return 0;
 }
